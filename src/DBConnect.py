fc38f7d1b32e6760bdf0594f3653d0839b813b19
il    iciuv <miguel.garcia.duch@gmail.com>
2024-04-10 20:07:06 +0200

    last

--- src/DBConnect.py
+++ src/DBConnect.py
@@ -1,17 +1,18 @@
 import psycopg2
 import streamlit as st
 
-def get_connection():
+
+def get_db_connection():
     """
     Establishes and returns a connection to the database.
     """
     try:
         connection = psycopg2.connect(
-            database="xdfipckk",
-            user="xdfipckk",
-            password="3XIvuo-lGwvQ4swylHeaGzY1S4s1MDGI",
-            host="ella.db.elephantsql.com",
-            port="5432"
+            database=st.secrets.postgresql["database"],
+            user=st.secrets.postgresql["user"],
+            password=st.secrets.postgresql["password"],
+            host=st.secrets.postgresql["host"],
+            port=st.secrets.postgresql["port"],
         )
         return connection
     except (Exception, psycopg2.Error) as error:
